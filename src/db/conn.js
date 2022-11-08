ba5d8dab71146fe88e079ad8957a69a46f962f89
Anitha <anithav0909@gmail.com>
2022-11-08 20:04:19 +0530

    file updated

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,6 +2,8 @@ const e = require("express");
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://anithav:anithav@ac-jpcphw9-shard-00-00.msc5chm.mongodb.net:27017,ac-jpcphw9-shard-00-01.msc5chm.mongodb.net:27017,ac-jpcphw9-shard-00-02.msc5chm.mongodb.net:27017/?ssl=true&replicaSet=atlas-7tblg2-shard-0&authSource=admin&retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
     
 }).then(()=>{
     console.log("connected successful");
