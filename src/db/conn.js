5f95518227fc6e8a227a0329308e7b60df82d17a
Anitha <anithav0909@gmail.com>
2022-11-08 15:50:20 +0530

    file updated

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const e = require("express");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/test",{
+mongoose.connect("mongodb://anithav:anithav@ac-jpcphw9-shard-00-00.msc5chm.mongodb.net:27017,ac-jpcphw9-shard-00-01.msc5chm.mongodb.net:27017,ac-jpcphw9-shard-00-02.msc5chm.mongodb.net:27017/?ssl=true&replicaSet=atlas-7tblg2-shard-0&authSource=admin&retryWrites=true&w=majority",{
     
 }).then(()=>{
     console.log("connected successful");
--- src/models/registers.js
+++ src/models/registers.js
@@ -44,7 +44,7 @@ const employeeSchema = new mongoose.Schema({
 employeeSchema.methods.generateAuthToken = async function(){
    try {
        console.log(this._id);
-       const token = jwt.sign({_id:this._id.toString()}, "mynameisgaddamidrusbashafromandhraoradesh");
+       const token = jwt.sign({_id:this._id.toString()}, "mynameisanithavfrombangaloreworkingincentelon");
        this.tokens = this.tokens.concat({token:token})
        await this.save();
        return token;
