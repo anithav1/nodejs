29925c2283b2d289b04a5215fc7afd31c949e9f8
Anitha <anithav0909@gmail.com>
2022-11-09 21:13:06 +0530

    file updated

--- src/app.js
+++ src/app.js
@@ -83,7 +83,7 @@ const token=await useremail.generateAuthToken();
 console.log("The token part "+token);
 
 if(isMatch){
-        res.status(201).render("dashboard");
+        res.status(201).render("views/dashboard.hbs");
     }
     else{
         res.send("Invalid login detail");
